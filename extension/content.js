84e8f3349f24d3a0c5483028721d6496bf26251f
Ilya Rodionov <ris58h@gmail.com>
2018-08-27 14:29:03 +0300

    Show comment's preview.

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,3 @@
-const YOUTUBE_TIMESTAMP_REGEX = /(^|\s)(((\d?\d):)?((\d\d))|\d):(\d\d)/
-
 if (window.location.pathname == '/watch') {
     const videoId = parseParams(window.location.href)['v']
     const fields = 'items(snippet(topLevelComment(snippet)))'
@@ -37,8 +35,53 @@ function processTimeComments(timeComments) {
         stamp.classList.add('__youtube-timestamps__stamp')
         stamp.style.left = (tc.time / video.duration * 100) + "%"
         bar.appendChild(stamp)
+
+        stamp.addEventListener('mouseenter', function() {
+            showPreview(tc.authorAvatar, tc.authorName, tc.text)
+        })
+        stamp.addEventListener('mouseleave', function () {
+            hidePreview()
+        })
     }
     container.appendChild(bar)
+}
+
+function showPreview(avatar, name, text) {
+    const parent = document.querySelector('.ytp-tooltip.ytp-preview')
+    let preview = document.querySelector('.__youtube-timestamps__preview')
+    if (!preview) {
+        preview = document.createElement('div')
+        preview.classList.add('__youtube-timestamps__preview')
+        parent.insertAdjacentElement('afterbegin', preview)
+
+        const authorElement = document.createElement('div')
+        authorElement.classList.add('__youtube-timestamps__preview__author')
+        preview.appendChild(authorElement)
+
+        const avatarElement = document.createElement('img')
+        avatarElement.classList.add('__youtube-timestamps__preview__avatar')
+        authorElement.appendChild(avatarElement)
+
+        const nameElement = document.createElement('span')
+        nameElement.classList.add('__youtube-timestamps__preview__name')
+        authorElement.appendChild(nameElement)
+
+        const textElement = document.createElement('div')
+        textElement.classList.add('__youtube-timestamps__preview__text')
+        preview.appendChild(textElement)
+    }
+    preview.style.display = ''
+    preview.style.width = parent.querySelector('.ytp-tooltip-bg').style.width
+    preview.querySelector('.__youtube-timestamps__preview__avatar').src = avatar
+    preview.querySelector('.__youtube-timestamps__preview__name').textContent = name
+    preview.querySelector('.__youtube-timestamps__preview__text').textContent = text
+}
+
+function hidePreview() {
+    let preview = document.querySelector('.__youtube-timestamps__preview')
+    if (preview) {
+        preview.style.display = 'none'
+    }
 }
 
 function parseParams(href) {
@@ -56,7 +99,7 @@ function parseParams(href) {
 }
 
 function extractTime(text) {
-    const ts = text.match(YOUTUBE_TIMESTAMP_REGEX)
+    const ts = text.match(/(^|\s)(((\d?\d):)?((\d\d))|\d):(\d\d)/)
     if (!ts || !ts[0]) {
         return null
     }
