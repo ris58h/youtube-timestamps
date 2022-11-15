e6cb34c37c3a9c39e263e35e61dfcaab18299f91
Ilya Rodionov <ris58h@gmail.com>
2022-11-15 17:21:27 +0400

    Get rid of fs-extra package

--- dist.js
+++ dist.js
@@ -1,17 +1,19 @@
-import fsExtraPkg from 'fs-extra'
-const { remove, ensureDir, readJson, createWriteStream } = fsExtraPkg
+import fs from 'fs'
 import archiver from 'archiver'
 
-{(async function() {
-    await remove("./dist")
-    await ensureDir("./dist")
+fs.rmSync("./dist", {recursive: true, force: true})
+fs.mkdirSync("./dist")
 
-    const name = process.env.npm_package_name
-    const version = (await readJson("./extension/manifest.json")).version
-    const output = createWriteStream(`./dist/${name}-${version}.zip`)
-    const archive = archiver("zip")
-    archive.pipe(output)
-    archive.glob("**/*", { cwd: "./extension" })
-    archive.on("error", err => { throw err })
-    archive.finalize()
-})()}
+const name = process.env.npm_package_name
+const version = readJsonSync("./extension/manifest.json").version
+const output = fs.createWriteStream(`./dist/${name}-${version}.zip`)
+const archive = archiver("zip")
+archive.pipe(output)
+archive.glob("**/*", { cwd: "./extension" })
+archive.on("error", err => { throw err })
+archive.finalize()
+
+function readJsonSync(path) {
+    const content = fs.readFileSync(path, "utf8")
+    return JSON.parse(content)
+}
