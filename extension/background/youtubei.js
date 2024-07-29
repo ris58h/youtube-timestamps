18843798b4815bb72f84763c5601b1d379034d1b
Ilya Rodionov <ris58h@gmail.com>
2024-07-29 20:03:57 +0300

    Manifest V3

--- extension/background/youtubei.js
+++ extension/background/youtubei.js
@@ -1,19 +1,6 @@
 const MAX_COMMENT_PAGES = 5
 const MAX_COMMENTS = 100
 
-// YouTube declines requests with wrong Origin.
-// We have to remove the Origin header which is added automatically by the browser.
-chrome.webRequest.onBeforeSendHeaders.addListener(
-    details => {
-        const newRequestHeaders = details.requestHeaders.filter(header => {
-            return header.name.toLowerCase() !== "origin"
-        })
-        return {requestHeaders: newRequestHeaders}
-    },
-    {urls: ["https://www.youtube.com/*"]},
-    ["blocking", "requestHeaders", chrome.webRequest.OnBeforeSendHeadersOptions.EXTRA_HEADERS].filter(Boolean)
-)
-
 const INNERTUBE_API_KEY = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
 const INNERTUBE_CLIENT_NAME = "WEB"
 const INNERTUBE_CLIENT_VERSION = "2.20211129.09.00"
